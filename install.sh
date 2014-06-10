09de8934b86b136aed68c1f23aadb753d73151a5
Simon Su <simonsu@mitac.com.tw>
2014-06-10 04:01:38 +0000

    add download ruls

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+cd /opt
+wget https://github.com/micloud/nomon/archive/master.zip
+unzip master.zip
+mv nomon-master nomon
+
+cd /opt/nomon
 echo "Install nomon to service..."
 cp ./nomon /etc/init.d/
 

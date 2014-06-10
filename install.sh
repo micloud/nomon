8789f78f97e896ff45f8d9c739837f99500be4d0
peihsinsu <simonsu.mail@gmail.com>
2014-06-10 22:56:42 +0800

    update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /opt
-wget https://github.com/micloud/nomon/archive/master.zip
+wget -O master.zip https://github.com/micloud/nomon/archive/master.zip
 unzip master.zip
 mv nomon-master nomon
 
@@ -13,5 +13,6 @@ chmod 755 /etc/init.d/nomon
 
 echo "Add symbolic link to rc*.d..."
 echo "chkconfig add..."
-chkconfig --add nomon
+chkconfig --add nomon && /etc/init.d/nomon start
 
+echo "Please check your filewall rule to open the service port (default 1337 port)..."
